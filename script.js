0259594e878a83cc974bdd0990fc576286133868
Kings-studio <67502766+Kings-studio@users.noreply.github.com>
2021-01-19 23:20:40 +0100

    New Updated site

--- script.js
+++ script.js
@@ -84,3 +84,24 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 6000); // Change image every 6 seconds
     }
+
+
+    
+
+    //Read more script
+    function myFunction() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more");
+        var btnText = document.getElementById("myBtn");
+
+        if (dots.style.display === "none"){
+            dots.style.display = "inline";
+            btnText.innerHTML = "Read more";
+            moreText.style.display =
+            "none";
+        } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Read less";
+            moreText.style.display = "inline";
+        }
+    }
